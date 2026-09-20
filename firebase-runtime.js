dc9f0f42bbb4254faf03c17a3da792ec2e00731b
tcss2026-byte <tcss2026@gmail.com>
2026-09-20 23:16:53 +0300

    Add files via upload

--- firebase-runtime.js
+++ firebase-runtime.js
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   projectId: "borh-d7a41",
   storageBucket: "borh-d7a41.firebasestorage.app",
   messagingSenderId: "935675116038",
-  appId: "1:935675116038:web:2e57c74710826288e26187",
-  measurementId: "G-SN88GJBXHH"
+  appId: "1:935675116038:web:525851ffbfc24016e26187",
+  measurementId: "G-L5QW8608MH"
 };
 export const ADMIN_EMAIL="tcss2026@gmail.com";
